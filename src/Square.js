a357e9251ef48ae14394b76bec055114b691dfa5
Cédric David <cedric.david@btopenworld.com>
2018-12-01 21:46:25 +1000

    add solution

--- src/Square.js
+++ src/Square.js
@@ -1,34 +1,60 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Square.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./Square.css";
 
 export default class Square extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-        };
-    }
-
-    render () {
-        const size = `${this.props.size}px`;
-        const style = {
-            width: size,
-            height: size,
-            backgroundColor: this.props.initialColor,
-        };
-        return (
-            <div className="SquareContainer">
-                <div style={style} className="Square" />
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.squareElem = null;
+    this.state = {
+      x: 0,
+      y: 0
+    };
+    this.squareRef = null;
+  }
+  updateCoords(e) {
+    const { clientX, clientY } = e;
+    const { top, left } = this.squareRef.getBoundingClientRect();
+    this.setState({
+      x: clientX - left,
+      y: clientY - top
+    });
+  }
+  resetCoords() {
+    this.setState({
+      x: 0,
+      y: 0
+    });
+  }
+  render() {
+    const size = `${this.props.size}px`;
+    const style = {
+      width: size,
+      height: size,
+      backgroundColor: this.props.initialColor
+    };
+    const { x, y } = this.state;
+    return (
+      <div className="SquareContainer">
+        <div
+          ref={e => (this.squareRef = e)}
+          style={style}
+          className="Square"
+          onMouseMove={e => this.updateCoords(e)}
+          onMouseOut={() => this.resetCoords()}
+        >
+          <div>{`${x}, ${y}`}</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 Square.propTypes = {
-    initialColor: PropTypes.string,
-    size: PropTypes.number.isRequired,
+  initialColor: PropTypes.string,
+  size: PropTypes.number.isRequired
 };
 
 Square.defaultProps = {
-    initialColor: 'blue',
+  initialColor: "blue"
 };
